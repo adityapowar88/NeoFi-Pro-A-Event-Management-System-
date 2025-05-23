8cbe05caff21c0112321ee2b4c67f6b2da747af4
adityapowar88 <adityapowar8888@gmail.com>
2025-05-23 14:52:26 +0530

    initial commit

--- neofi-backend/core/urls.py
+++ neofi-backend/core/urls.py
@@ -24,21 +24,3 @@ urlpatterns = [
     path('api/collaboration/', include('collaboration.urls')),
     path('api/versioning/', include('versioning.urls')),
 ]
-
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Event Versioning API",
-#         default_version='v1',
-#         description="API for event versioning, rollback, changelogs, and more.",
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
-
-# urlpatterns = [
-#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-# ]
